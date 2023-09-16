051c4e305dea53acea25092c23cd7fcf292a6935
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-09-15 17:11:55 -0700

    Update Decode-the-morse-code.js

--- Decode-the-morse-code.js
+++ Decode-the-morse-code.js
@@ -23,3 +23,13 @@ as input and return a decoded human-readable string.
 
 
 // Solution
+
+decodeMorse = function(morseCode){
+  function decodeMorseLetter(letter) {
+    return MORSE_CODE[letter];
+  }
+  function decodeMorseWord(word) {
+    return word.split(' ').map(decodeMorseLetter).join('');
+  }
+  return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+}
